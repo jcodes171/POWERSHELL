7919778f94d923685dee5bf082105d9c894b09e6
jcodes171 <135860726+jcodes171@users.noreply.github.com>
2024-11-17 21:12:50 -0600

    Update GetRoomsByDisplayName.ps1

--- EXCHANGE_ONLINE/Room_Mailboxes/GetRoomsByDisplayName.ps1
+++ EXCHANGE_ONLINE/Room_Mailboxes/GetRoomsByDisplayName.ps1
@@ -1,4 +1,3 @@
 ##############################################################################
-##############################################################################
 ##Get RoomMailBoxes by Display Name -update "-Identity" parameter as needed##
 Get-Mailbox -Identity "*keywordordisplayname*" -Filter '(RecipientTypeDetails -eq "RoomMailBox")'| Select-Object Name, Alias
