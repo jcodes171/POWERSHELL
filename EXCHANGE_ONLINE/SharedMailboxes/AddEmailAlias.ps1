0d74f96fa60c23eea8824cb4ef08f09b556b333b
jcodes171 <135860726+jcodes171@users.noreply.github.com>
2024-11-17 21:14:01 -0600

    Update AddEmailAlias.ps1

--- EXCHANGE_ONLINE/SharedMailboxes/AddEmailAlias.ps1
+++ EXCHANGE_ONLINE/SharedMailboxes/AddEmailAlias.ps1
@@ -1,4 +1,2 @@
-##################################################################################
 ######Add email address alias to Mailbox######
-##################################################################################
 Set-Mailbox -Identity "mailbox@example.com" -EmailAddresses SMTP:mailbox@example.com,smtp:alias@example.com
